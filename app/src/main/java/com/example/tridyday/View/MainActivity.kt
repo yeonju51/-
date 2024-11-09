4a5e1a8835cdeee444da6cd222bc1bfa543aef6d
jih023 <jih023@naver.com>
2024-11-09 11:41:04 +0900

    Feat: 홈 화면 구현 initial: first commit

---
+++ app/src/main/java/com/example/tridyday/View/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tridyday.View
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tridyday.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/example/tridyday/ViewModel/viewModel.kt
@@ -0,0 +1,4 @@
+package com.example.tridyday.ViewModel
+
+class viewModel {
+}
