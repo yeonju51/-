b220001afad2fb3996854b45b6e07cebcf85bae4
jih023 <jih023@naver.com>
2024-11-20 20:41:46 +0900

    Fix: 일정 등록 화면 수정

--- app/src/main/java/com/example/tridyday/Model/Schedule.kt
+++ app/src/main/java/com/example/tridyday/Model/Schedule.kt
@@ -2,6 +2,7 @@ package com.example.tridyday.Model
 
 data class Schedule(val title: String,
                     val startTime: String,
-                    val endTime: String) {
+                    val endTime: String,
+                    val memo: String) {
 
 }
