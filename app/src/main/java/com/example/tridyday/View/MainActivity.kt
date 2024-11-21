6c35847c78c751b4396423c0cf4e8a163b7067f1
aa2859 <aa2859@naver.com>
2024-11-21 13:55:54 +0900

    Fix: MainActivity.kt 복구

--- app/src/main/java/com/example/tridyday/View/MainActivity.kt
+++ app/src/main/java/com/example/tridyday/View/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.example.tridyday.View
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.tridyday.R
+import com.example.tridyday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        binding.botNav.setupWithNavController(navController)
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.homeFragment || destination.id == R.id.registTripFragment) {
+                binding.botNav.visibility = View.GONE
+            } else {
+                binding.botNav.visibility = View.VISIBLE
+            }
+        }
+
+        setContentView(binding.root)
+
+
+
 
     }
+
 }
