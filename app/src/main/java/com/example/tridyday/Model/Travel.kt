05b28f7850a70f7ea45b682faea1bbbba6376f37
aa2859 <aa2859@naver.com>
2024-12-02 15:30:39 +0900

    FIx: var로 변환

--- app/src/main/java/com/example/tridyday/Model/Travel.kt
+++ app/src/main/java/com/example/tridyday/Model/Travel.kt
@@ -3,24 +3,24 @@ package com.example.tridyday.Model
 import android.net.Uri
 
 data class Travel(
-    val title: String,
-    val location: String,
-    val startDate: String?,
-    val endDate: String?,
-    val photoUri: Uri? = null // 사진 URI를 추가
+    var title: String,
+    var location: String,
+    var startDate: String?,
+    var endDate: String?,
+    var photoUri: Uri? = null // 사진 URI를 추가
 ) {
     data class Schedule(
-        val title: String,
-        val startTime: Int,
-        val endTime: Int,
-        val memo: String
+        var title: String,
+        var startTime: Int,
+        var endTime: Int,
+        var memo: String
     ) {
-        data class locate(
-            val name: String,
-            val iD: String,
-            val lat: Double,
-            val lng: Double,
-            val place: String
+        data class Locate(
+            var name: String,
+            var id: String,
+            var lat: Double,
+            var lng: Double,
+            var place: String
         )
     }
 }
